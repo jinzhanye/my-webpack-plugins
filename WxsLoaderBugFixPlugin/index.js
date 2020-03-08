b8b9f7dda19bc3e6075b94e056f7386664a3d5c9
叶同学 <jinzhanye9@gmail.com>
2020-03-08 17:27:44 +0800

    fix

--- WxsLoaderBugFixPlugin/index.js
+++ WxsLoaderBugFixPlugin/index.js
@@ -1,7 +1,7 @@
 // 修复 "@tinajs/wxs-loader": "1.2.7" 打包后 wxs 路径中出现 node_modules 的 BUG
-module.exports = class PageBackupPlugin {
+module.exports = class WxsLoaderBugFixPlugin {
   apply(compiler) {
-    compiler.hooks.emit.tap('WsxLoaderBugFixPlugin', (compilation) => {
+    compiler.hooks.emit.tap('WxsLoaderBugFixPlugin', (compilation) => {
       const { assets } = compilation
       Object.entries(assets).forEach(([filePath, fileContent]) => {
         if (filePath.startsWith('wxs/_/_/node_modules/')) {
